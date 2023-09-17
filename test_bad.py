99c40dd71468c2b071f2480129ef84231dddfa30
nikita <dayzwaitik123@yandex.ru>
2023-09-17 22:14:55 +0300

    Test validation of all values are nu,bers

--- test_bad.py
+++ test_bad.py
@@ -14,6 +14,12 @@ data_det_bad = (
     (Matrix([[1, 2], [3, 5], [2, 3]]).determinate, None),
 )
 
+data_create_str = (
+    [['a', 'b'], ['a', 'b']],
+    [['a'], ['b']],
+    ['kek']
+)
+
 
 @pytest.mark.parametrize('matrix', data_create_bad)
 def test_create_bad(matrix):
@@ -24,3 +30,9 @@ def test_create_bad(matrix):
 @pytest.mark.parametrize('matrix_det, expected_det', data_det_bad)
 def test_det_bad(matrix_det, expected_det):
     assert matrix_det == expected_det
+
+
+@pytest.mark.parametrize('matrix', data_create_str)
+def test_create_from_string_error(matrix):
+    with pytest.raises(TypeError):
+        Matrix(matrix)

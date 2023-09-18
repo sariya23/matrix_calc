c6c945f2b5a06a87e2c19df0a63a6d40e92d0d62
nikita <dayzwaitik123@yandex.ru>
2023-09-18 23:52:32 +0300

    Add inverse matrix

--- utils.py
+++ utils.py
@@ -99,3 +99,42 @@ def transpose_matrix(matrix):
     return res
 
 
+def get_element_minor(matrix, i, j):
+    minor = []
+
+    for row in (matrix[:i] + matrix[i + 1:]):
+        minor.append(row[:j] + row[j + 1:])
+
+    return determinant_recursive(minor)
+
+
+def get_matrix_minor(matrix):
+    matrix_minor = [[0] * len(matrix[0]) for i in range(len(matrix))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            matrix_minor[i][j] = get_element_minor(matrix, i, j)
+
+    return matrix_minor
+
+
+def get_matrix_algebraic_complements(matrix_minor):
+    matrix_algebraic_complements = [[0] * len(matrix_minor[0]) for i in range(len(matrix_minor))]
+
+    for i in range(len(matrix_minor)):
+        for j in range(len(matrix_minor[0])):
+            matrix_algebraic_complements[i][j] = ((-1) ** (i + j)) * matrix_minor[i][j]
+
+    return matrix_algebraic_complements
+
+
+def inverse_matrix(matrix):
+    if len(matrix) != len(matrix[0]):
+        raise TypeError('This matrix can not inverse')
+    if determinant_recursive(matrix) == 0:
+        raise TypeError('Determinate of matrix equal 0')
+
+    det = determinant_recursive(matrix)
+    matrix_algebraic_complements = get_matrix_algebraic_complements(get_matrix_minor(matrix))
+
+    return mul_number_to_matrix(1 / det, transpose_matrix(matrix_algebraic_complements))
